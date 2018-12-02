82ec0e020b30a40e0caf9d8aa2119691c58ee7c0
Jorge Ceja <jorgeceja123@gmail.com>
2018-12-01 17:45:46 -0800

    [refactor] clean up folder structure

--- src/serve.py
+++ src/serve.py
@@ -2,13 +2,13 @@ from sys import platform as _platform
 import json
 from flask import Flask, jsonify, render_template, request
 
-from modHero import ModHero
+from src.modHero import ModHero
 
 # model path
 PWD = '/var/www/html/flaskapp'
-MODEL_PATH = './model/model.json'
+MODEL_PATH = './src/model/model.json'
 # PWD + '/data/model.json'
-WEIGHTS_PATH = './model/model.h5'
+WEIGHTS_PATH = './src/model/model.h5'
 # PWD + '/data/model.h5'
 
 # preprocessing
@@ -17,11 +17,11 @@ MAXLEN = 100
 LABELS = ["toxic", "severe_toxic", "obscene",
           "threat", "insult", "identity_hate"]
 
-# HTTP API
-app = Flask(__name__)
-
 # load modHero class (model + self contained helper functions)
 modHero = ModHero(MODEL_PATH, WEIGHTS_PATH, MAXLEN, LABELS)
+
+# HTTP API
+app = Flask(__name__)
 
 
 def run_inference_on_text(text):
